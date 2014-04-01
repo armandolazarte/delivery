53694ddbcac9ff9f9fcc4e8934dd1f9c9ecb3aae
Marco Vilera <mvilera@gmail.com>
2014-04-01 15:12:05 -0430

    asd

--- app/views/includes/navbar.blade.php
+++ app/views/includes/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 	<div class="container">
-        <ul class="nav navbar-nav navbar-right"><strong>
+        <ul class="nav navbar-nav navbar-right">
           	<li><a href="index.html">Inicio</a></li>
           	<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Contabilidad<b class="caret"></b></a>
           		<ul class="dropdown-menu">
@@ -12,7 +12,6 @@
         	<li><a href="index.html">Contabilidad</a></li>
         	<li><a href="index.html">Reportes</a></li>
         	<li><a href="index.html">Soporte</a></li>
-        </strong>
         </ul>
     </div>
 </nav>
