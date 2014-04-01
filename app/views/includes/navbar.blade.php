11e5407a6a93699d9d6245a0d6082c92680bba6e
Marco Vilera <mvilera@gmail.com>
2014-04-01 14:51:11 -0430

    modificación de menú

--- app/views/includes/navbar.blade.php
+++ app/views/includes/navbar.blade.php
@@ -1,11 +1,9 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 	<div class="container">
-		<p class="navbar-text">Menú</p>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="index.html">Inicio</a></li>
-          <li><a href="index.html"></a></li>
-          <li><a href="index.html">Projects</a></li>
-          <li><a href="index.html">The Team</a></li>
+          <li><a href="index.html">Contabilidad</a></li>
+          <li><a href="index.html">Reportes</a></li>
           <li><a href="index.html">Soporte</a></li>
         </ul>
     </div>
