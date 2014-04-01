0c660106d0e0f4d7a825f64ad5420e9c37e232e1
Marco Vilera <mvilera@gmail.com>
2014-04-01 12:20:08 -0430

    nav bar

---
+++ app/views/includes/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav id="navigation">
+	<div class="container">
+        <ul class="navlinks">
+          <li><a href="index.html">Homepage</a></li>
+          <li><a href="index.html">About Us</a></li>
+          <li><a href="index.html">Projects</a></li>
+          <li><a href="index.html">The Team</a></li>
+          <li><a href="index.html">Contacts</a></li>
+        </ul>
+    </div>
+</nav>
