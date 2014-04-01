6cde3240eb442b20872597f0a229fdf70a739d67
Marco Vilera <mvilera@gmail.com>
2014-04-01 11:07:59 -0430

    added bootstrap3.1.1

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -1,3 +1,4 @@
 <meta charset="UTF-8">
 <title>Aplicación para manejo de contabilidad</title>
 <link rel="stylesheet" type="text/css" href="css/normalize.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
