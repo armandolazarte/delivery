c6d59d88be106479fa09c79aeed1fd08e6ef1e0d
Marco Vilera <mvilera@gmail.com>
2014-04-01 14:47:13 -0430

    navbar fix

--- app/views/includes/navbar.blade.php
+++ app/views/includes/navbar.blade.php
@@ -1,11 +1,12 @@
-<nav class="navbar navbar-default" role="navigation">
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 	<div class="container">
+		<p class="navbar-text">Menú</p>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="index.html">Homepage</a></li>
-          <li><a href="index.html">About Us</a></li>
+          <li><a href="index.html">Inicio</a></li>
+          <li><a href="index.html"></a></li>
           <li><a href="index.html">Projects</a></li>
           <li><a href="index.html">The Team</a></li>
-          <li><a href="index.html">Contacts</a></li>
+          <li><a href="index.html">Soporte</a></li>
         </ul>
     </div>
 </nav>
