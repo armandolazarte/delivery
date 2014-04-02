1ea20b8cecc925b2963847ca008800a23c4a03bb
Marco Vilera <mvilera@gmail.com>
2014-04-02 17:55:39 -0430

    asd

--- app/views/ventas/agregar.blade.php
+++ app/views/ventas/agregar.blade.php
@@ -20,50 +20,50 @@
                     <div class="col-md-8 well">
                     <form role="form" class="form form-horizontal">
                         <div class="form-group">
-                            <label class="control-label col-md-3 text-muted" for="form_ventas_rif">RIF: </label>
+                            <label class="control-label col-md-4 text-muted" for="form_ventas_rif">RIF: </label>
                             <div class="col-md-5">
                                 <input type="text" class="form-control input-sm" name="ventas_rif" id="form_ventas_rif">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-md-3 text-muted" for="form_ventas_nombre">Razón social: </label>
+                            <label class="control-label col-md-4 text-muted" for="form_ventas_nombre">Razón social: </label>
                             <div class="col-md-5">
                                 <input type="text" class="form-control input-sm" name="ventas_nombre" id="form_ventas_nombre">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-md-3 text-muted" for="form_ventas_control">Número de control: </label>
+                            <label class="control-label col-md-4 text-muted" for="form_ventas_control">Número de control: </label>
                             <div class="col-md-5">
                                 <input type="text" class="form-control input-sm" name="ventas_control" id="form_ventas_control">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-md-3 text-muted" for="form_ventas_factura">Número de factura: </label>
+                            <label class="control-label col-md-4 text-muted" for="form_ventas_factura">Número de factura: </label>
                             <div class="col-md-5">
                                 <input type="text" class="form-control input-sm" name="ventas_factura" id="form_ventas_factura">
                             </div> 
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-md-3 text-muted" for="form_ventas_fecha_fact">Fecha de facturación: </label>
+                            <label class="control-label col-md-4 text-muted" for="form_ventas_fecha_fact">Fecha de facturación: </label>
                             <div class="col-md-5">
                                 <input type="text" class="form-control input-sm" name="ventas_fecha_fact" id="form_ventas_fecha_fact">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="control-label col-md-3 text-muted" for="form_ventas_reg_maq">Nº Registro de máquina: </label>
+                            <label class="control-label col-md-4 text-muted" for="form_ventas_reg_maq">Nº Registro de máquina: </label>
                             <div class="col-md-5">
                                 <input type="text" class="form-control input-sm" name="ventas_reg_maq" id="form_ventas_reg_maq">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-md-3 text-muted" for="form_ventas_nota_deb">Nº Nota de débito: </label>
+                            <label class="control-label col-md-4 text-muted" for="form_ventas_nota_deb">Nº Nota de débito: </label>
                             <div class="col-md-5">
                                 <input type="text" class="form-control input-sm" name="ventas_nota_deb" id="form_ventas_nota_deb">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-md-3 text-muted" for="form_ventas_nota_cred">Nº Nota de crédito: </label>
+                            <label class="control-label col-md-4 text-muted" for="form_ventas_nota_cred">Nº Nota de crédito: </label>
                             <div class="col-md-5">
                                 <input type="text" class="form-control input-sm" name="ventas_nota_cred" id="form_ventas_nota_cred">
                             </div>
