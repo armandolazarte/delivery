e2e9a76abb4d4c42b3d8f30a7715f579ad3e59dc
Marco Vilera <mvilera@gmail.com>
2014-04-02 15:51:56 -0430

    asd

--- app/views/ventas/agregar.blade.php
+++ app/views/ventas/agregar.blade.php
@@ -10,25 +10,25 @@
 			</ul>
         </div>
         <div class="col-md-9">
-            <form role="form" class="form form-inline">
+            <form role="form" class="form form-horizontal">
                 <div class="form-group">
-                    <label for="form_ventas_rif">RIF</label>
-                    <input type="text" class="form-control" name="ventas_rif" id="form_ventas_rif">
+                    <label class="control-label" for="form_ventas_rif">RIF</label>
+                    <input type="text" class="form-control input-sm" name="ventas_rif" id="form_ventas_rif">
                 </div>
                 <div class="form-group">
-                    <label for="form_ventas_nombre">Nombre o razón social</label>
+                    <label class="control-label" for="form_ventas_nombre">Nombre o razón social</label>
                     <input type="text" class="form-control" name="ventas_nombre" id="form_ventas_nombre">
                 </div>
                 <div class="form-group">
-                    <label for="form_ventas_control">Número de control</label>
+                    <label class="control-label" for="form_ventas_control">Número de control</label>
                     <input type="text" class="form-control" name="ventas_control" id="form_ventas_control">
                 </div>
                 <div class="form-group">
-                    <label for="form_ventas_factura">Número de factura</label>
+                    <label class="control-label" for="form_ventas_factura">Número de factura</label>
                     <input type="text" class="form-control" name="ventas_factura" id="form_ventas_factura">
                 </div>
                 <div class="form-group">
-                    <label for="form_ventas_fecha_fact">Fecha de facturación</label>
+                    <label class="control-label" for="form_ventas_fecha_fact">Fecha de facturación</label>
                     <input type="text" class="form-control" name="ventas_fecha_fact" id="form_ventas_fecha_fact">
                 </div>
             </form>
