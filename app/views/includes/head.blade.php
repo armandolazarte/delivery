1b2481375647949f57cf8def060044d92af74f99
Marco Vilera <mvilera@gmail.com>
2014-04-02 14:42:23 -0430

    asd

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
 <title>Aplicación para manejo de contabilidad</title>
-<link rel="stylesheet" type="text/css" href="css/normalize.css">
+<link rel="stylesheet" type="text/css" href="//css/normalize.css">
 <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/custom.css">
+<link rel="stylesheet" type="text/css" href="//css/custom.css">
