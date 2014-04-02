ffdde63ab857094972f29f7fc86d7ccc62719ae5
Marco Vilera <mvilera@gmail.com>
2014-04-02 14:39:39 -0430

    asd

--- app/controllers/VentasController.php
+++ app/controllers/VentasController.php
@@ -3,10 +3,13 @@ class VentasController extends BaseController {
 	
 	protected $layout = 'layout.default';
 
+	public function getIndex() {
+		return "hola mundo";
+	}
+
 	public function getAgregar() {
 		$this->layout->content = View::make('ventas.agregar');
 	}
-
 	
 }
 ?>
