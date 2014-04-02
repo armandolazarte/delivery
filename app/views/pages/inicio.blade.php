b0f318d1f8e6dd375e8ce5e44903784bbb7aad3b
Marco Vilera <mvilera@gmail.com>
2014-04-02 10:06:59 -0430

    asd

--- app/views/pages/inicio.blade.php
+++ app/views/pages/inicio.blade.php
@@ -2,15 +2,23 @@
 
 @section('content')
 	<div class="row">
-        <div class="col-md-2">
-            <ul class="nav nav-stacked">
+        <div class="col-md-3">
+            <ul class="nav nav-tabs nav-stacked">
 				<li><a href="#">Agregar facturas</a></li>
 				<li><a href="#">Eliminar facturas</a></li>
 				<li><a href="#">Modificar facturas</a></li>
 			</ul>
         </div>
-        <div class="col-md-10">
-            <table></table>
+        <div class="col-md-9">
+            <table class="table">
+            	<thead>
+            		<th>ID</th>
+            		<th>N° de Control</th>
+            		<th>N° de Factura</th>
+            		<th>Nombre</th>
+            		<th>Monto</th>
+            	</thead>
+            </table>
         </div>
     </div>   
 @stop
