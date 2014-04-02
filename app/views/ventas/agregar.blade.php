8d43fad7f9dce25241a859677dcb008123e2d48d
Marco Vilera <mvilera@gmail.com>
2014-04-02 16:38:15 -0430

    fff

--- app/views/ventas/agregar.blade.php
+++ app/views/ventas/agregar.blade.php
@@ -12,31 +12,31 @@
             <div class="panel-body">
             <form role="form" class="form form-horizontal well">
                 <div class="form-group">
-                    <label class="control-label col-md-3 text-muted" for="form_ventas_rif">RIF</label>
+                    <label class="control-label col-md-3 text-muted" for="form_ventas_rif">RIF: </label>
                     <div class="col-md-5">
                         <input type="text" class="form-control input-sm" name="ventas_rif" id="form_ventas_rif">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-md-3 text-muted" for="form_ventas_nombre">Razón social</label>
+                    <label class="control-label col-md-3 text-muted" for="form_ventas_nombre">Razón social: </label>
                     <div class="col-md-5">
                         <input type="text" class="form-control input-sm" name="ventas_nombre" id="form_ventas_nombre">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-md-3 text-muted" for="form_ventas_control">Número de control</label>
+                    <label class="control-label col-md-3 text-muted" for="form_ventas_control">Número de control: </label>
                     <div class="col-md-5">
                         <input type="text" class="form-control input-sm" name="ventas_control" id="form_ventas_control">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-md-3 text-muted" for="form_ventas_factura">Número de factura</label>
+                    <label class="control-label col-md-3 text-muted" for="form_ventas_factura">Número de factura: </label>
                     <div class="col-md-5">
                         <input type="text" class="form-control input-sm" name="ventas_factura" id="form_ventas_factura">
                     </div> 
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-md-3 text-muted" for="form_ventas_fecha_fact">Fecha de facturación</label>
+                    <label class="control-label col-md-3 text-muted" for="form_ventas_fecha_fact">Fecha de facturación: </label>
                     <div class="col-md-5">
                         <input type="text" class="form-control input-sm" name="ventas_fecha_fact" id="form_ventas_fecha_fact">
                     </div>
