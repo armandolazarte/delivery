810429bfb282a8af11f776d83077c10104b470cb
Marco Vilera <mvilera@gmail.com>
2014-04-02 09:41:35 -0430

    add invoices

--- app/views/includes/navbar.blade.php
+++ app/views/includes/navbar.blade.php
@@ -2,14 +2,13 @@
 	<div class="container">
         <ul class="nav navbar-nav navbar-right">
           	<li><a href="index.html">Inicio</a></li>
-          	<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Contabilidad<b class="caret"></b></a>
+          	<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Contabilidad <b class="caret"></b></a>
           		<ul class="dropdown-menu">
             		<li><a href="#">Facturas de venta</a></li>
             		<li><a href="#">Facturas de compra</a></li>
             		<li><a href="#">Notas de crédito</a></li>
           		</ul>
         	</li>
-        	<li><a href="index.html">Contabilidad</a></li>
         	<li><a href="index.html">Reportes</a></li>
         	<li><a href="index.html">Soporte</a></li>
         </ul>
--- app/views/pages/inicio.blade.php
+++ app/views/pages/inicio.blade.php
@@ -1,6 +1,14 @@
 @extends('layout.default')
+@section('sidebar')
+	<ul class="nav nav-stacked">
+		<li><a>Agregar facturas</a></li>
+		<li><a>Eliminar facturas</a></li>
+		<li><a>Modificar facturas</a></li>
+	</ul>
+@stop
+
 @section('content')
-	<div>
-		que cagada
+	<div id="add_frame">
+		cualquier vaina
 	</div>
 @stop
