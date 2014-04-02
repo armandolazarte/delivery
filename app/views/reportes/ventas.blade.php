2f2ca662cb74756306ed68a5ce31884544718b97
Marco Vilera <mvilera@gmail.com>
2014-04-02 10:59:37 -0430

    asd

---
+++ app/views/reportes/ventas.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.default')
+
+@section('content')
+	<table class="table table-stripped table-condensed">
+       	<thead>
+       		<th>ID</th>
+       		<th>N° de Control</th>
+       		<th>N° de Factura</th>
+       		<th>Nombre</th>
+       		<th>Monto</th>
+       	</thead>
+       	<tbody>
+       		<tr>
+       			<td>1</td>
+       			<td>45000</td>
+       			<td>48999</td>
+       			<td>Importadora La Villa de las Mascotas, C.A.</td>
+       			<td>45.000</td>
+       		</tr>
+       	</tbody>
+       	<tfoot>
+       	</tfoot>
+    </table>
+@stop
+
